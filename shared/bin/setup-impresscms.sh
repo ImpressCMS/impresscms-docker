cdff93977e4522148728f21b303446dca47cf1a3
Raimondas Rimkevičius <github@mekdrop.name>
2022-09-22 17:38:33 +0300

    Update setup-impresscms.sh

--- shared/bin/setup-impresscms.sh
+++ shared/bin/setup-impresscms.sh
@@ -2,13 +2,7 @@
 
 set -e
 
-RUNNING_MODE=$(cat /etc/mode)
-
 pushd /srv/www
-
-if [ "$RUNNING_MODE" == "dev" ]; then
-  composer install 2>&1
-fi;
 
 echo "Migrating..."
 ./bin/phoenix migrate -vv 2>&1 | indent.sh
